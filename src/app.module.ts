f091a4111578a735015e3d5521df228e3ba32799
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-03-18 19:34:26 -0300

    agregue el modulo ciudad pero no me creo las entitys

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadModule } from './ciudad/ciudad.module';
+
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password:'1234',
+      database: 'escuela',
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    CiudadModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
